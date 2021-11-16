e9ff70cc54a4410b7a10c51ca190e9922e99a601
Hugo Gruson <hugo.gruson@protonmail.com>
2021-11-16 15:46:45 +0100

    Create get_user_id()

---
+++ R/get_user_id.R
@@ -0,0 +1,36 @@
+#' Get slack user ID from their name
+#'
+#' @param name A character string with the name ("Firstname Lastname")
+#' @param token The OAuth token to access
+#' [slack users.list API endpoint](https://api.slack.com/methods/users.list)
+#'
+#' @importFrom httr2 request req_auth_bearer_token req_perform resp_body_json
+#' @importFrom dplyr %>% bind_rows
+#' @importFrom purrr pluck map
+#'
+#' @export
+#'
+get_user_id <- function(name, token) {
+
+  all_users <- request("https://slack.com/api/users.list") %>%
+    req_auth_bearer_token(token) %>%
+    req_perform() %>%
+    resp_body_json()
+
+  all_ids <- all_users %>%
+    pluck("members") %>%
+    map(~ c("id" = .x[["id"]], "email" = .x$profile$email)) %>%
+    bind_rows()
+
+  target_email <- name %>%
+    strsplit(" ") %>%
+    pluck(1) %>%
+    setNames(c("firstname", "lastname")) %>%
+    glue::glue_data("{firstname}.{lastname}@lshtm.ac.uk") %>%
+    tolower()
+
+  target_id <- all_ids$id[which(tolower(all_ids$email) == target_email)]
+
+  return(target_id)
+
+}
