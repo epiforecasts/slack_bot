49d8a2c456d5bed174aba2515470e849df33db62
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2025-07-28 09:53:43 +0100

    add a prompt when announcing meeting

---
+++ R/get_prompt.R
@@ -0,0 +1,15 @@
+#' Get a random prompt
+#'
+#' @importFrom glue glue
+#'
+#' @export
+#' @autoglobal
+#'
+#' @examples
+#' get_prompt()
+#'
+get_prompt <- function() {
+  prompts <- readLines(file.path("data-raw", "prompts.txt"))
+  prompt <- sample(prompts, 1)
+  glue("Now: {prompt}")
+}
